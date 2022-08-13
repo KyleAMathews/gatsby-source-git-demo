b7b0be138968bec69b096f6e31668a9722497e24
Kyle Mathews <mathews.kyle@gmail.com>
2022-08-13 11:52:45 -0700

    update demo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,5 @@
 import type { GatsbyConfig } from "gatsby"
+import path from "path"
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -9,7 +10,28 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: `md-files`,
+        remote: `https://github.com/KyleAMathews/md-files`,
+        branch: `main`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    // This is a bit hacky — you have to load gatsby-source-filesystem so
+    // transformers can load files from gatsby-source-git as source-git assumes
+    // source-filesystem is loaded.
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.resolve(`./src`),
+        // Ignore everything.
+        ignore: [`**`],
+      },
+    },
+  ],
 }
 
 export default config
